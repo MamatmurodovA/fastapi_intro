513b43ec29e74221c081e82fab4c05b51cc51f9a
azam.mamatmurodov <azam.mamatmurodov@gmail.com>
2022-12-14 19:05:43 +0500

    added example with get query and swagger doc

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+from typing import Union
+
 from fastapi import FastAPI
 
-app = FastAPI()
+my_awesome_api = FastAPI()
 
 
-@app.get("/")
+@my_awesome_api.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@my_awesome_api.get('/items/foo')
+async def foo_view(name: str = 'Michael'):
+    """
+    Hi, there!
+    It's foo page, and we'll return dummy text.
+    :return:
+    """
+    return "Hello, {}! It's our foo page".format(name)
+
+
